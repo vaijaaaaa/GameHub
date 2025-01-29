273d63ddd5057dee0331884a55d96863648a5e87
vaijaaaaa <vaijuwalker111@gmail.com>
2025-01-29 23:41:43 +0530

    bugs fix

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,7 +5,7 @@ function ListGroup(){
     <li className="list-group-item">A second item</li>
     <li className="list-group-item">A third item</li>
     <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
+    <li className="list-group-item">And a fifth onee</li>
   </ul>
   
     );
