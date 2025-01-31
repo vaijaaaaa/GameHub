598aba19644f9907880bd37851affe97d60a4715
vaijaaaaa <vaijuwalker111@gmail.com>
2025-01-31 22:31:37 +0530

    props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,13 @@
 function ListGroup(){
 
-  const items = [];
+  const items = ["banglore","Delhi"];
 
     
       return (
         <>
-        
+        <h1>List</h1>
       <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth onee</li>
+      {items.map((item)=>(<li key = {item}>{item}</li>))}
         </ul>
         
         </>
