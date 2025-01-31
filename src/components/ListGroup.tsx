ab69b59a3c729cb127904d263a8154a9c8c567f5
vaijaaaaa <vaijuwalker111@gmail.com>
2025-01-31 22:23:01 +0530

    group

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
 function ListGroup(){
-    return (
-    <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth onee</li>
-  </ul>
-  
-    );
+    <div>
+      return (
+      <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth onee</li>
+        </ul>
+      );
+    </div>
 }
 
 export default ListGroup;
